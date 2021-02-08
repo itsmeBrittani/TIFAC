e51099e799905141460d4bae2a35c4568f3b4df7
Brittani Savage <brittanisavage@gmail.com>
2021-02-08 11:58:58 -0600

    Basic set up complete

---
+++ BlackJack/app.js
@@ -0,0 +1,26 @@
+/*
+[] - make the deck
+    -> give each card value
+    -> Ace can be 1 or 11
+[] - player starts
+    -> hit & stand
+[] - dealer goes
+    -> dealer must hit under 17
+[] - first to or closest to 21 wins
+[] - over 21 is BUST, game over, loser
+[] - make deck with factory? put in array of objects?
+
+*/
+
+
+$(() => {
+console.log("you got it dude!")
+
+
+
+
+
+
+
+
+});
