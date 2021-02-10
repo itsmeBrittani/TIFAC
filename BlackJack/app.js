2389672b63c1f1f6e88fe48fd1b7a5e906269ca8
Brittani Savage <brittanisavage@gmail.com>
2021-02-10 10:38:11 -0600

    Made acecheck and bustcheck

--- BlackJack/app.js
+++ BlackJack/app.js
@@ -21,9 +21,33 @@ for(i = 0; i <$value.length; i++){
 }
 
 console.log($deck)
+console.log($deck[5])
 
 let $playerHand = [];
 let $houseHand = [];
+
+let $playerTotal = 0;
+let $houseTotal = 0;
+
+//checks if any Aces are in hand
+const aceCheck = () =>{
+    if ($playerTotal > 21 && $playerHand.contains($deck[48] || $deck[49] || $deck[50] || $deck[51])){
+        $deck[48].point = 1;
+        $deck[49].point = 1;
+        $deck[50].point = 1;
+        $deck[51].point = 1;
+    } 
+    bustCheck()
+}
+
+const bustCheck = () => {
+    aceCheck()
+    if($playerTotal > 21){
+        alert("Player busted! House wins!");
+    }else if($houseTotal > 21){
+        alert("House busted! Player wins!")
+    }
+}
 
 // click event listener for Deal
 const $dealBtn = $('#deal')
