1ff760885d59fb94d3db891f75e8f9b291793e6b
Brittani Savage <brittanisavage@gmail.com>
2021-03-01 10:50:46 -0600

    Database and Index Set up

---
+++ TIFAC/controllers/TIFAC.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const router = express.Router();
+const Incident = require('../models/TIFAC.js');
+
+//Routes
+//seed
+
+router.get('/seed', async (req, res) => {
+    const newReports =
+        [
+            {
+            establishment: 'Insight Studios',
+            location: '1062 N Milwaukee Ave, Chicago, IL',
+            date: '01/06/2021',
+            description: 'Participated in the riot at the Capitol in January 2021',
+            type: ['racism', 'domestic terrorism']
+        }, {
+            establishment: 'Tank Noodle',
+            location: '4953-55 N Broadway, Chicago, IL, 60640',
+            date: '01/06/2021',
+            description: 'Participated in the riot at the Capitol in January 2021',
+            type: ['domestic terrorism']
+        }, {
+            establishment: 'Chicago\'s Best Barbershop',
+            location: '2318 N California Ave, Chicago, IL',
+            date: '01/06/2021',
+            description: 'Participated in the riot at the Capitol in January 2021',
+            type: ['domestic terrorism']
+        }
+    ]
+
+    try {
+        const seedItems = await Incident.create(newReports)
+        res.send(seedItems)
+    } catch (err) {
+        res.send('This is not what I want');
+    }
+});
+
+
+//index
+router.get('/', (req, res) => {
+    Incident.find({}, (err, reports) => {
+    res.render('index.ejs', {
+        allReports: reports
+    });
+    });
+});
+
+//new
+router.get('/new', (req, res) => {
+    res.send('These are the new things')
+})
+
+//create
+
+
+//show
+router.get('/:id', (req, res) => {
+    Incident.findById(req.params.id, (err, foundReport) => {
+        res.render('show.ejs', {
+            report: foundReport
+        });
+    });
+});
+
+//edit
+
+
+
+//update
+
+
+//destroy
+
+
+module.exports = router;
---
+++ TIFAC/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const app = express();
+const port = 3000;
+const dbName = 'TIFAC';
+
+const reportController = require('./controllers/TIFAC.js');
+
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
+app.use('/TIFAC', reportController);
+
+mongoose.connect(`mongodb://localhost:27017/${dbName}`, {useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.once('open', () => {
+    console.log('connected to mongo');
+});
+
+
+//LISTENER
+app.listen(port, () => {
+    console.log("Ready for your orders, Goddess!!");
+})
